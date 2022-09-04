87c60125b84dc169b06ff4275cf1f1befc0a0626
Gabriel4g <gabrielprogramador11@gmail.com>
2022-09-03 20:57:50 -0500

    edit layout from users

--- js/main.js
+++ js/main.js
@@ -1,48 +1,55 @@
-const inputUser = document.querySelector("#user-input");
-const buttonGet = document.querySelector("#btn-buscar");
-const imgUser = document.querySelector("#img-user");
-const containerInfos = document.querySelector("#usuarios");
-const textUser = document.querySelector(".username");
-const textFollowers = document.querySelector(".followers");
-const textFollowing = document.querySelector(".following");
-const textURL = document.querySelector(".url");
-const textLocation = document.querySelector(".location");
-const textName = document.querySelector(".name");
-const textRepos = document.querySelector(".repos");
-const textType = document.querySelector(".type");
-const textTwitter = document.querySelector(".Twitter");
-const textEmail = document.querySelector(".email");
+const inputUser = document.querySelector("#user-input"),
+    buttonGet = document.querySelector("#btn-buscar"),
+    imgUser = document.querySelector("#img-user"),
+    containerInfos = document.querySelector("#usuarios"),
+    textUser = document.querySelector(".username"),
+    textFollowers = document.querySelector(".followers"),
+    textFollowing = document.querySelector(".following"),
+    textURL = document.querySelector(".url"),
+    textLocation = document.querySelector(".location"),
+    textName = document.querySelector(".name"),
+    textRepos = document.querySelector(".repos"),
+    textCompany = document.querySelector(".company"),
+    textTwitter = document.querySelector(".Twitter"),
+    textDate = document.querySelector(".bio");
 
 async function getDatafromAPI(user) {
-    const API = await fetch(`https://api.github.com/users/${user}`);
 
-    if (API.status == 200) {
-        const data = await API.json();
-        return data;
+    try {
+        const API = await fetch(`https://api.github.com/users/${user}`);
+
+        if (API.status == 200) {
+            const data = await API.json();
+            return data;
+        }
+    }
+
+    catch (error) {
+        console.log(error);
     }
 }
 
 async function renderInfoFromUsers(user) {
     const data = await getDatafromAPI(user);
 
+    console.log(data);
     if (data) {
         imgUser.src = data.avatar_url;
-        textUser.innerHTML = `${data.login} <hr/>`;
-        textFollowers.innerHTML = `Followers: ${data.followers}`;
-        textFollowing.innerHTML = `Following: ${data.following}`;
+        textUser.innerHTML = `${data.login}`;
+        textDate.innerHTML = `${data.bio}`
+        textFollowers.innerHTML = `${data.followers}`;
+        textFollowing.innerHTML = `${data.following}`;
 
         textURL.setAttribute("href", `${data.html_url}`);
-        textURL.innerHTML = "URL from User"
+        textURL.innerHTML = 'Profile URL'
 
         containerInfos.style.display = "flex";
 
-        textLocation.innerHTML = `Location: ${data.location}`;
-        textName.innerHTML = `Name: ${data.name}`;
-        textRepos.innerHTML = `Repositories: ${data.public_repos}`;
-        textType.innerHTML = `Type: ${data.type}`;
-        textTwitter.innerHTML = `Twitter: ${data.twitter_username}`;
-        textEmail.innerHTML = `Email: ${data.email}`;
-        console.log(data);
+        textLocation.innerHTML = `${data.location}`;
+        textName.innerHTML = `${data.name}`;
+        textRepos.innerHTML = `${data.public_repos}`;
+        textCompany.innerHTML = `${data.company || 'Not found'}`;
+        textTwitter.innerHTML = `${data.twitter_username || "Not found"}`;
     }
 
     else {
